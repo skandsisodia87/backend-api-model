68402c2add0c79821b84712b529365dba0343764
Skand Sisodia <sisodiaskand@gmail.com>
2024-04-09 00:30:24 +0530

    config/connect DB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from "express"
+import connectDB from "./db/db.js";
 import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 
+connectDB()
 
-
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is running at port: ${port}`)
 })
